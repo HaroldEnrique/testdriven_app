db8a9bf6e538d8d64fbf0b95668d2d8bf29fa4e6
harold <haroldcotacallapa@upeu.edu.pe>
2020-09-20 02:15:12 +0000

    testing setup

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,10 +1,28 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db #new
 
+import sys
+import unittest
 
 cli = FlaskGroup(app)
 
+
+#new
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command()
+def test():
+    """Ejecuta las pruebas sin cobertura de código"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 if __name__ == '__main__':
     cli()
