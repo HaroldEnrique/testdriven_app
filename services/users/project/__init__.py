460c0ad753764ae3710e4c4ab3c2a1bd4ada014a
harold <haroldcotacallapa@upeu.edu.pe>
2020-09-20 01:25:54 +0000

    container service_db running

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,12 +1,35 @@
+# services;users;project;__init__.py
+
+import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # nuevo
 
-
-# instantiate the app
+# instanciando la app
 app = Flask(__name__)
 
 api = Api(app)
 
+# establecer configuración
+# app.config.from_object("project.config.DevelopmentConfig") #new
+
+app_settings = os.getenv('APP_SETTINGS') #new
+app.config.from_object(app_settings)     #new
+
+# instanciado la db
+db = SQLAlchemy(app)  # nuevo
+
+# modelo
+class User(db.Model):  # nuevo
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 class UsersPing(Resource):
     def get(self):
@@ -15,5 +38,8 @@ class UsersPing(Resource):
         'message': 'pong!'
     }
 
+
+# import sys
+# print(app.config, file=sys.stderr)
 
 api.add_resource(UsersPing, '/users/ping')
